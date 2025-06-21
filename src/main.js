2131215320b22f76c9c719f6355525e18a81d776
eduwang <145199866+eduwang@users.noreply.github.com>
2025-06-22 02:19:51 +0900

    SweetAlert2 추가

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+import Swal from 'sweetalert2';
+
 document.getElementById('warningButton').addEventListener('click', () => {
-  alert('I Said Do Not Click This Button');
+  Swal.fire({
+    title: 'Warning!',
+    text: 'I Said Do Not Click This Button',
+    icon: 'warning',
+    confirmButtonText: 'Okay...',
+    background: '#1b263b',
+    color: '#e0e1dd',
+    confirmButtonColor: '#415a77'
+  });
 });
